ce379625f75b4193ec1a0781db2e0d4bf608ef0c
Cyan244466666 <cammellryan@gmail.com>
2021-11-30 10:20:24 +0000

    initial upload.

---
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,17 @@
+﻿// Rory Clark - https://rory.games - 2019
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField]
+    float m_health = 20f;
+
+    public void DamageEnemy(float _damage)
+    {
+        m_health -= _damage;
+        if(m_health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -0,0 +1,43 @@
+﻿// Rory Clark - https://rory.games - 2019
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject m_bulletPrefab = null;
+
+    [SerializeField]
+    Transform m_spawnPosition = null;
+
+    [SerializeField]
+    KeyCode m_fireKey = KeyCode.Space;
+
+    [SerializeField]
+    float m_fireRate = 0.25f;
+    float m_currentFireTimer = 0f;
+
+    void Update()
+    {
+        if (m_currentFireTimer > 0)
+        {
+            m_currentFireTimer -= Time.deltaTime;
+        }
+
+        // GetKeyDown and GetKey will happen at different times
+        // GetKeyDown will happen as soon as the user presses the key
+        // GetKey will work when it is held
+        if ((Input.GetKeyDown(m_fireKey) || Input.GetKey(m_fireKey)) && m_currentFireTimer <= 0)
+        {
+            m_currentFireTimer = m_fireRate;
+            SpawnBullet();
+        }
+    }
+
+    void SpawnBullet()
+    {
+        GameObject go = Instantiate(m_bulletPrefab);
+        go.transform.position = m_spawnPosition.position;
+    }
+   
+}
