8648ae13139aeb4484f87f4b08fb1531e1a02d8c
Cyan244466666 <cammellryan@gmail.com>
2021-12-03 10:08:42 +0000

    added 'AsteroidSpawner' script and working on asteroid class implementation

--- Assets/Scripts/Asteroids/Asteroid.cs
+++ Assets/Scripts/Asteroids/Asteroid.cs
@@ -1,16 +1,47 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Diagnostics;
+using UnityEngine;
 
 namespace Asteroids {
-    public class Asteroid {
+    public class Asteroid : MonoBehaviour {
         private float Health { get; }
         private Vector3 Scale { get; }
         private float Velocity { get; }
+        private float ScoreValue { get; }
+        private AsteroidType AsteroidType { get; }
     
         // Constructor:
-        public Asteroid(float health, Vector3 scale, float velocity) {
-            Health = health;
-            Scale = scale;
-            Velocity = velocity;
+        public Asteroid(AsteroidType asteroidType) {
+            // Here we need a way to assign all the asteroids properties on initialisation
+            // based on an enum input param.
+            switch (asteroidType) {
+                case AsteroidType.A1:
+                    Health = 2f;
+                    break;
+                case AsteroidType.A2:
+                    break;
+                case AsteroidType.A3:
+                    break;
+                case AsteroidType.A4:
+                    break;
+                case AsteroidType.A5:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(asteroidType), asteroidType, null);
+            }
+        }
+
+        private void Awake() {
+            // Enemy Scale Spawn Function
         }
     }
+
+    public enum AsteroidType {
+        A1,
+        A2,
+        A3,
+        A4,
+        A5
+    };
+    
 }
---
+++ Assets/Scripts/Asteroids/AsteroidSpawner.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class AsteroidSpawner : MonoBehaviour {
+    
+    [SerializeField] private const GameObject AsteroidPrefab = null;
+    [SerializeField] private float currentTime;
+    [SerializeField] private const float MINSpawnTime = 0.2f;
+    [SerializeField] private const float MAXSpawnTime = 1.5f;
+    [SerializeField] private const float MAXSpawnWidth = 1.5f;
+    [SerializeField] private const float MINSpawnWidth = 0.2f;
+
+    private void Update() {
+        
+        currentTime -= Time.deltaTime;
+        if (currentTime <= 0) {
+            currentTime = RandomSpawnTime(MINSpawnTime, MAXSpawnTime);
+            SpawnAsteroid();
+        }
+
+    }
+
+    // Randomly generates a spawn time for asteroid:
+    private static float RandomSpawnTime(float min, float max) {
+        return Random.Range(min, max);
+    }
+
+    // Spawns an asteroid:
+    private void SpawnAsteroid() {
+        // Here we need to generate which asteroid type we want to spawn!
+        var newAsteroid = Instantiate(AsteroidPrefab, transform, true);
+        newAsteroid.transform.localPosition = new Vector3(0f, 0f, Random.Range(MINSpawnWidth, MAXSpawnWidth));
+    }
+}
